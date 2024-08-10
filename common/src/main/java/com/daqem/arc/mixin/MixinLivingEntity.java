4b4a4a890e93c639248b2fdaa20b9b70038ea484
DAQEM <daqemyt@gmail.com>
2024-08-10 15:28:27 +0200

    Added Config Lib.

--- common/src/main/java/com/daqem/arc/mixin/MixinLivingEntity.java
+++ common/src/main/java/com/daqem/arc/mixin/MixinLivingEntity.java
@@ -1,8 +1,8 @@
 package com.daqem.arc.mixin;
 
 import com.daqem.arc.api.action.result.ActionResult;
-import com.daqem.arc.event.triggers.PlayerEvents;
 import com.daqem.arc.api.player.ArcServerPlayer;
+import com.daqem.arc.event.triggers.PlayerEvents;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
@@ -21,26 +21,20 @@ public abstract class MixinLivingEntity extends Entity {
         super(entityType, level);
     }
 
-    private LivingEntity getLivingEntity() {
-        return (LivingEntity) (Object) this;
-    }
-
     @Inject(at = @At("RETURN"), method = "addEffect(Lnet/minecraft/world/effect/MobEffectInstance;Lnet/minecraft/world/entity/Entity;)Z")
     private void addEffect(MobEffectInstance effect, Entity entity, CallbackInfoReturnable<Boolean> cir) {
-        if (getLivingEntity() instanceof ArcServerPlayer serverPlayer) {
-            MobEffectInstance mobEffectInstance2 = getLivingEntity().getActiveEffectsMap().get(effect.getEffect());
-            if (mobEffectInstance2 != null) {
-                if (entity != null) {
-                    if (entity instanceof ServerPlayer source) {
-                        if (source.getName().getString().equals("a")) {
-                            return;
-                        }
+        final LivingEntity self = (LivingEntity) (Object) this;
+        if (self instanceof ArcServerPlayer serverPlayer) {
+            if (self.getActiveEffectsMap().containsKey(effect.getEffect())) {
+                if (entity instanceof ServerPlayer source) {
+                    if (source.getName().getString().equals("a")) {
+                        return;
                     }
                 }
             }
             ActionResult actionResult = PlayerEvents.onEffectAdded(serverPlayer, effect, entity);
             if (actionResult.shouldCancelAction()) {
-                getLivingEntity().removeEffect(effect.getEffect());
+                self.removeEffect(effect.getEffect());
             }
         }
     }
